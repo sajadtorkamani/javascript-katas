cc377189d5858ccd210e008178ce0c56c3980456
Sajad Torkamani <sajadtorkamani1@gmail.com>
2018-09-21 16:00:16 +0100

    Solve kata

--- solution.js
+++ solution.js
@@ -1,66 +1,23 @@
 /**
- * The constructor.
+ * Determine if a given set is a subset of another.
  *
- * @param matrix {array}
- * @constructor
+ * @param s1 {Set}
+ * @param s2 {Set}
+ * @returns {boolean}
  */
-export function Matrix(matrix) {
-  this.matrix = matrix;
-}
-
-/**
- * Get the diagonal from the top left to the bottom right.
- *
- * @returns {array}
- */
-Matrix.prototype.getTopLeftDiagonal = function () {
-  let diagonal = [];
-
-  for (let i = 0; i < this.matrix.length; i++) {
-    diagonal.push(this.matrix[i][i]);
-  }
-
-  return diagonal;
+export const isSubsetOf = (s1, s2) => {
+  return Array
+    .from(s1)
+    .every(val => s2.has(val));
 };
 
 /**
- * Get the diagonal from the bottom left to the top right.
+ * Determine if a given set is a superset of another.
  *
- * @returns {array}
+ * @param s1 {Set}
+ * @param s2 {Set}
+ * @returns {boolean}
  */
-Matrix.prototype.getBottomLeftDiagonal = function () {
-  let diagonal = [];
-
-  for (let row = this.matrix.length - 1, column = 0; row >= 0; row--, column++) {
-    diagonal.push(this.matrix[row][column]);
-  }
-
-  return diagonal;
-};
-
-/**
- * Return matrix with the diagonals reversed.
- *
- * @returns {array}
- */
-Matrix.prototype.getWithReversedDiagonals = function () {
-  let reversedMatrix = this.matrix.slice();
-  let topLeftDiagonal = this.getTopLeftDiagonal();
-  let bottomLeftDiagonal = this.getBottomLeftDiagonal();
-
-  // Reverse top left to bottom right diagonal.
-  for (let i = 0; i < this.matrix.length; i++) {
-    reversedMatrix[i][i] = topLeftDiagonal.pop();
-  }
-
-  // Reverse bottom left to top right diagonal.
-  for (let row = this.matrix.length - 1, column = 0; row >= 0; row--, column++) {
-    reversedMatrix[row][column] = bottomLeftDiagonal.pop();
-  }
-
-  return reversedMatrix;
-};
-
-export const reverseOnDiagonals = matrix => {
-  return new Matrix(matrix).getWithReversedDiagonals();
+export const isSupersetOf = (s1, s2) => {
+  return isSubsetOf(s2, s1);
 };
--- test.js
+++ test.js
@@ -1,77 +1,15 @@
-import { Matrix, reverseOnDiagonals } from './solution';
+import { isSubsetOf, isSupersetOf } from './solution';
 
-describe('Matrix', () => {
-  describe('constructor', () => {
-    test('sets this.matrix to argument', () => {
-      const m = new Matrix('foo');
-
-      expect(m.matrix).toEqual('foo');
-    });
+describe('isSubsetOf', () => {
+  test('returns true if s1 is a subset of s2', () => {
+    expect(isSubsetOf(new Set([1, 2, 3]), new Set([1, 2, 3, 4]))).toBe(true);
+    expect(isSubsetOf(new Set([1, 2, 3]), new Set([1, 2, 4, 5]))).toBe(false);
   });
+});
 
-  describe('getTopLeftDiagonal', () => {
-    test('returns correct diagonal', () => {
-      const matrix = new Matrix([
-        [43, 455, 32, 103],
-        [102, 988, 298, 981],
-        [309, 21, 53, 64],
-        [2, 22, 35, 291]
-      ]);
-
-      expect(matrix.getTopLeftDiagonal()).toEqual([43, 988, 53, 291]);
-    });
-  });
-
-  describe('getBottomLeftDiagonal', () => {
-    test('returns correct diagonal', () => {
-      const matrix = new Matrix([
-        [43, 455, 32, 103],
-        [102, 988, 298, 981],
-        [309, 21, 53, 64],
-        [2, 22, 35, 291]
-      ]);
-
-      expect(matrix.getBottomLeftDiagonal()).toEqual([2, 21, 298, 103]);
-    });
-  });
-
-  describe('getWithReversedDiagonals', () => {
-    test('returns matrix with its diagonals reversed', () => {
-      const matrix = new Matrix([
-        [43, 455, 32, 103],
-        [102, 988, 298, 981],
-        [309, 21, 53, 64],
-        [2, 22, 35, 291]
-      ]);
-
-      const expected = [
-        [291, 455, 32, 2],
-        [102, 53, 21, 981],
-        [309, 298, 988, 64],
-        [103, 22, 35, 43]
-      ];
-
-      expect(matrix.getWithReversedDiagonals()).toEqual(expected);
-    });
-  });
-
-  describe('reverseOnDiagonals', () => {
-    test('returns matrix with its diagonals reversed', () => {
-      const matrix = [
-        [43, 455, 32, 103],
-        [102, 988, 298, 981],
-        [309, 21, 53, 64],
-        [2, 22, 35, 291]
-      ];
-
-      const expected = [
-        [291, 455, 32, 2],
-        [102, 53, 21, 981],
-        [309, 298, 988, 64],
-        [103, 22, 35, 43]
-      ];
-
-      expect(reverseOnDiagonals(matrix)).toEqual(expected);
-    });
+describe('isSupersetOf', () => {
+  test('returns true if s1 is a superset of s2', () => {
+    expect(isSupersetOf(new Set([1, 2, 3, 4]), new Set([1, 2]))).toBe(true);
+    expect(isSupersetOf(new Set([1, 2, 3, 4]), new Set([1, 2, 5]))).toBe(false);
   });
 });
