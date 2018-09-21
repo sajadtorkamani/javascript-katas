f8f30c8d088c8b75644d54c8bc50f4670506afe7
Sajad Torkamani <sajadtorkamani1@gmail.com>
2018-09-21 15:37:42 +0100

    Solve kata

--- solution.js
+++ solution.js
@@ -1,5 +1,66 @@
-export const average = (...args) => {
-  const sum = args.reduce((a, b) => a + b);
+/**
+ * The constructor.
+ *
+ * @param matrix {array}
+ * @constructor
+ */
+export function Matrix(matrix) {
+  this.matrix = matrix;
+}
 
-  return sum / args.length;
+/**
+ * Get the diagonal from the top left to the bottom right.
+ *
+ * @returns {array}
+ */
+Matrix.prototype.getTopLeftDiagonal = function () {
+  let diagonal = [];
+
+  for (let i = 0; i < this.matrix.length; i++) {
+    diagonal.push(this.matrix[i][i]);
+  }
+
+  return diagonal;
+};
+
+/**
+ * Get the diagonal from the bottom left to the top right.
+ *
+ * @returns {array}
+ */
+Matrix.prototype.getBottomLeftDiagonal = function () {
+  let diagonal = [];
+
+  for (let row = this.matrix.length - 1, column = 0; row >= 0; row--, column++) {
+    diagonal.push(this.matrix[row][column]);
+  }
+
+  return diagonal;
+};
+
+/**
+ * Return matrix with the diagonals reversed.
+ *
+ * @returns {array}
+ */
+Matrix.prototype.getWithReversedDiagonals = function () {
+  let reversedMatrix = this.matrix.slice();
+  let topLeftDiagonal = this.getTopLeftDiagonal();
+  let bottomLeftDiagonal = this.getBottomLeftDiagonal();
+
+  // Reverse top left to bottom right diagonal.
+  for (let i = 0; i < this.matrix.length; i++) {
+    reversedMatrix[i][i] = topLeftDiagonal.pop();
+  }
+
+  // Reverse bottom left to top right diagonal.
+  for (let row = this.matrix.length - 1, column = 0; row >= 0; row--, column++) {
+    reversedMatrix[row][column] = bottomLeftDiagonal.pop();
+  }
+
+  return reversedMatrix;
+};
+
+export const reverseOnDiagonals = matrix => {
+  return new Matrix(matrix).getWithReversedDiagonals();
 };
--- test.js
+++ test.js
@@ -1,7 +1,77 @@
-import { average } from './solution';
+import { Matrix, reverseOnDiagonals } from './solution';
 
-describe('average', () => {
-  test('returns average of all arguments', () => {
-    expect(average(2, 4, 6)).toBe(4);
+describe('Matrix', () => {
+  describe('constructor', () => {
+    test('sets this.matrix to argument', () => {
+      const m = new Matrix('foo');
+
+      expect(m.matrix).toEqual('foo');
+    });
+  });
+
+  describe('getTopLeftDiagonal', () => {
+    test('returns correct diagonal', () => {
+      const matrix = new Matrix([
+        [43, 455, 32, 103],
+        [102, 988, 298, 981],
+        [309, 21, 53, 64],
+        [2, 22, 35, 291]
+      ]);
+
+      expect(matrix.getTopLeftDiagonal()).toEqual([43, 988, 53, 291]);
+    });
+  });
+
+  describe('getBottomLeftDiagonal', () => {
+    test('returns correct diagonal', () => {
+      const matrix = new Matrix([
+        [43, 455, 32, 103],
+        [102, 988, 298, 981],
+        [309, 21, 53, 64],
+        [2, 22, 35, 291]
+      ]);
+
+      expect(matrix.getBottomLeftDiagonal()).toEqual([2, 21, 298, 103]);
+    });
+  });
+
+  describe('getWithReversedDiagonals', () => {
+    test('returns matrix with its diagonals reversed', () => {
+      const matrix = new Matrix([
+        [43, 455, 32, 103],
+        [102, 988, 298, 981],
+        [309, 21, 53, 64],
+        [2, 22, 35, 291]
+      ]);
+
+      const expected = [
+        [291, 455, 32, 2],
+        [102, 53, 21, 981],
+        [309, 298, 988, 64],
+        [103, 22, 35, 43]
+      ];
+
+      expect(matrix.getWithReversedDiagonals()).toEqual(expected);
+    });
+  });
+
+  describe('reverseOnDiagonals', () => {
+    test('returns matrix with its diagonals reversed', () => {
+      const matrix = [
+        [43, 455, 32, 103],
+        [102, 988, 298, 981],
+        [309, 21, 53, 64],
+        [2, 22, 35, 291]
+      ];
+
+      const expected = [
+        [291, 455, 32, 2],
+        [102, 53, 21, 981],
+        [309, 298, 988, 64],
+        [103, 22, 35, 43]
+      ];
+
+      expect(reverseOnDiagonals(matrix)).toEqual(expected);
+    });
   });
 });
