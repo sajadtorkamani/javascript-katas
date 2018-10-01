32aa3a826ec7993641ad465fdb3cbaab38f93ad2
Sajad Torkamani <sajadtorkamani1@gmail.com>
2018-10-01 13:08:36 +0100

    Solve kata

--- solution.js
+++ solution.js
@@ -1,52 +1,7 @@
-/**
- * Reverse order of array elements
- *
- * @param arr
- * @returns {Array}
- */
-export const reverseOrder = arr => arr.reverse();
-
-/**
- * Reverse lines within string.
- *
- * @param str
- * @returns {string}
- */
-export const reverseLines = str => {
+export const switcheroo = str => {
   return str
-    .split('\n')
-    .reverse()
-    .join('\n');
-};
-
-/**
- * Reverse the sections within a string.
- *
- * @param str {string}
- * @returns {string}
- */
-export const reverseSections = str => {
-  return str.match(/([a-z]+)|([0-9]+)|([^a-z0-9]+)/gi)
-    .map(section => section.split('').reverse().join(''))
-    .join('');
-};
-
-export const reverseAll = arr => {
-  let result;
-
-  // Reverse order
-  result = reverseOrder(arr);
-
-  // Reverse lines
-  result = result.map(reverseLines);
-
-  // Reverse sections within the each line.
-  result = result.map(element => {
-    return element
-      .split('\n')
-      .map(reverseSections)
-      .join('\n');
-  });
-
-  return result;
+    .replace(/a/g, 'A')
+    .replace(/b/g, 'B')
+    .replace(/A/g, 'b')
+    .replace(/B/g, 'a')
 };
--- test.js
+++ test.js
@@ -1,33 +1,8 @@
-import { reverseAll, reverseLines, reverseOrder, reverseSections } from './solution';
+import { switcheroo } from './solution';
 
-describe('reverseOrder', () => {
-  test('reverses order of elements', () => {
-    expect(reverseOrder(['foo', 'bar', 'baz'])).toEqual(['baz', 'bar', 'foo']);
-  });
-});
-
-describe('reverseLines', () => {
-  test('reverses lines in string', () => {
-    expect(reverseLines('abc\n123\n!!!')).toEqual('!!!\n123\nabc');
-  });
-});
-
-describe('reverseSections', () => {
-  test('reverses sections within string', () => {
-    expect(reverseSections('xyz886abc#$%')).toEqual('zyx688cba%$#');
-  });
-});
-
-describe('reverseAll', () => {
+describe('switcheroo', () => {
   test('returns correct result', () => {
-    expect(reverseAll(
-      ["abc123!@#\n314159", "987bbc\n#$%\nxyz886"]
-    )).toEqual(
-      ["zyx688\n%$#\n789cbb", "951413\ncba321#@!"]
-    );
+    expect(switcheroo('abc')).toEqual('bac');
+    expect(switcheroo('aaaaccccbbbb')).toEqual('bbbbccccaaaa');
   });
-});
-
-// Test.assertSimilar(reverseAll(["abc123!@#\n314159","987bbc\n#$%\nxyz886"]),["zyx688\n%$#\n789cbb","951413\ncba321#@!"])
-// Test.assertSimilar(reverseAll(["JOVmG$&&?@\nJcE91632PXFhMAsRli\n30620DOXPZoO149\n5513??%-&@YUpyCmRUAO","_@!?#^15915736199076984"]),
-// ["^#?!@_48967099163751951","3155@&-%??OAURmCypUY\n02603OoZPXOD941\nEcJ23619ilRsAMhFXP\nGmVOJ@?&&$"])
+})
