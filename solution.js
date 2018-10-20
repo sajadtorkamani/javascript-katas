61d3e3e8b0f310268b75c3cc1818a8891132afb3
Sajad Torkamani <sajadtorkamani1@gmail.com>
2018-10-20 11:42:39 +0100

    Solve kata

--- solution.js
+++ solution.js
@@ -1,8 +1,14 @@
-export const smallEnough = (nums, threshold) => {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > threshold) {
-      return false;
-    }
-  }
-  return true;
-};
+export const gematria = str => {
+  const letterValues = {
+    a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, k: 10,
+    l: 20, m: 30, n: 40, o: 50, p: 60, q: 70, r: 80, s: 90, t: 100,
+    u: 200, x: 300, y: 400, z: 500, j: 600, v: 700, w: 900
+  };
+
+  return str
+    .replace(/ /g, '')
+    .toLowerCase()
+    .split('')
+    .map(char => letterValues[char])
+    .reduce((acc, val) => acc + val);
+}
--- test.js
+++ test.js
@@ -1,12 +1,11 @@
 import assert from 'assert';
-import { smallEnough } from './solution';
+import { gematria } from './solution';
 
-describe('smallEnough', () => {
+describe('gematria', () => {
   test('returns correct result', () => {
-    assert.equal(smallEnough([66, 101], 200), true);
-    assert.equal(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100), false);
-    assert.equal(smallEnough([101, 45, 75, 105, 99, 107], 107), true);
-    assert.equal(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120), true);
+    assert.equal(gematria("love"), 775);
+    assert.equal(gematria("Devil"), 738);
+    assert.equal(gematria("Coding is fun"), 458);
   });
 });
 
