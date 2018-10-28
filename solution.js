9824e0f1b19d3a864b66fe509db0116098984d75
Sajad Torkamani <sajadtorkamani1@gmail.com>
2018-10-28 10:43:15 +0000

    Solve kata

--- solution.js
+++ solution.js
@@ -1,14 +1,12 @@
-export const gematria = str => {
-  const letterValues = {
-    a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, k: 10,
-    l: 20, m: 30, n: 40, o: 50, p: 60, q: 70, r: 80, s: 90, t: 100,
-    u: 200, x: 300, y: 400, z: 500, j: 600, v: 700, w: 900
-  };
+export const getLetterPosition = letter => 'abcdefghijklmnopqrstuvwxyz'.indexOf(letter) + 1;
 
+export const alphaSeq = str => {
   return str
-    .replace(/ /g, '')
     .toLowerCase()
     .split('')
-    .map(char => letterValues[char])
-    .reduce((acc, val) => acc + val);
+    .sort()
+    .map(letter => {
+      return letter.toUpperCase() + letter.repeat(getLetterPosition(letter) - 1);
+    })
+    .join(',');
 }
--- test.js
+++ test.js
@@ -1,11 +1,25 @@
 import assert from 'assert';
-import { gematria } from './solution';
+import { getLetterPosition, alphaSeq } from './solution';
 
-describe('gematria', () => {
-  test('returns correct result', () => {
-    assert.equal(gematria("love"), 775);
-    assert.equal(gematria("Devil"), 738);
-    assert.equal(gematria("Coding is fun"), 458);
+describe('getLetterPosition', function () {
+  test('returns correct result', function () {
+    assert.equal(getLetterPosition('c'), 3);
   });
 });
+
+describe('alphaSeq', function () {
+  test('returns correct result', function () {
+    assert.equal(alphaSeq("ZpglnRxqenU"),
+      "Eeeee,Ggggggg,Llllllllllll,Nnnnnnnnnnnnnn,Nnnnnnnnnnnnnn,Pppppppppppppppp,Qqqqqqqqqqqqqqqqq,Rrrrrrrrrrrrrrrrrr,Uuuuuuuuuuuuuuuuuuuuu,Xxxxxxxxxxxxxxxxxxxxxxxx,Zzzzzzzzzzzzzzzzzzzzzzzzzz");
+    assert.equal(alphaSeq("NyffsGeyylB"),
+      "Bb,Eeeee,Ffffff,Ffffff,Ggggggg,Llllllllllll,Nnnnnnnnnnnnnn,Sssssssssssssssssss,Yyyyyyyyyyyyyyyyyyyyyyyyy,Yyyyyyyyyyyyyyyyyyyyyyyyy,Yyyyyyyyyyyyyyyyyyyyyyyyy");
+    assert.equal(alphaSeq("MjtkuBovqrU"),
+      "Bb,Jjjjjjjjjj,Kkkkkkkkkkk,Mmmmmmmmmmmmm,Ooooooooooooooo,Qqqqqqqqqqqqqqqqq,Rrrrrrrrrrrrrrrrrr,Tttttttttttttttttttt,Uuuuuuuuuuuuuuuuuuuuu,Uuuuuuuuuuuuuuuuuuuuu,Vvvvvvvvvvvvvvvvvvvvvv");
+    assert.equal(alphaSeq("EvidjUnokmM"),
+      "Dddd,Eeeee,Iiiiiiiii,Jjjjjjjjjj,Kkkkkkkkkkk,Mmmmmmmmmmmmm,Mmmmmmmmmmmmm,Nnnnnnnnnnnnnn,Ooooooooooooooo,Uuuuuuuuuuuuuuuuuuuuu,Vvvvvvvvvvvvvvvvvvvvvv");
+    assert.equal(alphaSeq("HbideVbxncC"),
+      "Bb,Bb,Ccc,Ccc,Dddd,Eeeee,Hhhhhhhh,Iiiiiiiii,Nnnnnnnnnnnnnn,Vvvvvvvvvvvvvvvvvvvvvv,Xxxxxxxxxxxxxxxxxxxxxxxx");
+  });
+});
+
 
