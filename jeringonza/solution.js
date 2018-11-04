d41b7cdddf9bda71a2da9effdde3fc9b48b0f398
Sajad Torkamani <sajadtorkamani1@gmail.com>
2018-11-04 10:23:05 +0000

    Solve kata

---
+++ jeringonza/solution.js
@@ -0,0 +1,9 @@
+export const jeringonza = str => {
+  const replaceVowel = vowel => {
+    const p = vowel === vowel.toUpperCase() ? 'P' : 'p';
+
+    return `${vowel}${p}${vowel}`;
+  };
+
+  return str.replace(/[aeiou]/gi, replaceVowel);
+};
