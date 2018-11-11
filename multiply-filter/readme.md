5d1aa996fd23675b073a6a5c8c461f15604aa45a
Sajad Torkamani <sajadtorkamani1@gmail.com>
2018-11-11 12:44:04 +0000

    Solve kata

---
+++ multiply-filter/readme.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/multiply-array-values-and-filter-non-numeric/train/javascript
---
+++ multiply-filter/solution.js
@@ -0,0 +1,5 @@
+export const multiplyAndFilter = (arr, multiplier) => {
+  return arr
+    .filter(v => typeof v === 'number')
+    .map(v => v * multiplier);
+};
