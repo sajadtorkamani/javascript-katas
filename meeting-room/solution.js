32362680c9a09cca310f4ee8717e82eba6befefe
Sajad Torkamani <sajadtorkamani1@gmail.com>
2018-12-02 11:47:11 +0000

    Solve kata

---
+++ meeting-room/solution.js
@@ -0,0 +1,5 @@
+export const meeting = rooms => {
+  const emptyRoomIndex = rooms.indexOf('O');
+
+  return emptyRoomIndex > -1 ? emptyRoomIndex : 'None available!';
+};
---
+++ meeting-room/test.js
@@ -0,0 +1,9 @@
+import { meeting } from './solution';
+
+describe('#meeting', () => {
+  test('returns correct result', () => {
+    expect(meeting(['X', 'O', 'X'])).toEqual(1);
+    expect(meeting(['O', 'X', 'X', 'X', 'X'])).toEqual(0);
+    expect(meeting(['X', 'X', 'X', 'X', 'X'])).toEqual('None available!');
+  });
+});
