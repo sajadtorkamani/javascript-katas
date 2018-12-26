50e83297f377d04199291108c7296e2ab761ffb4
Sajad Torkamani <sajadtorkamani1@gmail.com>
2018-12-26 08:20:27 +0000

    Solve kata

---
+++ bingo/readme.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/bingo-or-not/train/javascript
---
+++ bingo/solution.js
@@ -0,0 +1,11 @@
+const bingo = nums => {
+  const letters = nums.map(num => String.fromCharCode(64 + num));
+
+  return letters.includes('B')
+    && letters.includes('I')
+    && letters.includes('N')
+    && letters.includes('G')
+    && letters.includes('O') ? 'WIN' : 'LOSE';
+};
+
+export default bingo;
