4b7c686c503f097a6eabadf69105b7e9819cfdb3
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-01-12 19:03:19 +0000

    Solve kata

---
+++ freq-seq/solution.js
@@ -0,0 +1,9 @@
+const freqSeq = (str, separator) => {
+  const letters = [...str];
+
+  return letters
+    .map(letter => letters.filter(v => v === letter).length)
+    .join(separator);
+};
+
+export default freqSeq;
