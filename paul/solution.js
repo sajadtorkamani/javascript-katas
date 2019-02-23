18f1182129b8a4ee6e2873b0baafd4eea4314dfd
Sajad Torkamani <sajad@surelygroup.com>
2019-02-23 13:53:31 +0000

    Solve kata

---
+++ paul/solution.js
@@ -0,0 +1,19 @@
+const paul = activities => {
+  const activityScores = {
+    'kata': 5,
+    'Petes kata': 10,
+    'eating': 1,
+    'life': 0
+  };
+
+  const score = activities
+    .map(activity => activityScores[activity])
+    .reduce((a, b) => a + b, 0);
+
+  if (score >= 100) { return 'Miserable!'; }
+  if (score >= 70) { return 'Sad!'; }
+  if (score >= 40) { return 'Happy!'; }
+  return 'Super happy!';
+};
+
+export default paul;
---
+++ paul/test.js
@@ -0,0 +1,9 @@
+import paul from './solution';
+
+describe('paul', () => {
+  test('returns correct result', () => {
+    expect(paul(['life', 'eating', 'life'])).toEqual('Super happy!');
+    expect(paul(['life', 'Petes kata', 'Petes kata', 'Petes kata', 'eating'])).toEqual('Super happy!');
+    expect(paul(['Petes kata', 'Petes kata', 'eating', 'Petes kata', 'Petes kata', 'eating'])).toEqual('Happy!');
+  });
+});
