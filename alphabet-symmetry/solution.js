6da04724aef8ff19dd42462240f5d749c1585434
Sajad Torkamani <sajad@surelygroup.com>
2019-02-23 15:13:48 +0000

    Solve kata

---
+++ alphabet-symmetry/solution.js
@@ -0,0 +1,11 @@
+export const getCorrectPositionsCount = word => {
+  return word
+    .toLowerCase()
+    .split('')
+    .filter((letter, index) => letter === String.fromCharCode(97 + index))
+    .length;
+};
+
+const solve = words => words.map(getCorrectPositionsCount);
+
+export default solve;
