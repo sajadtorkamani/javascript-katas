c8eb1ee51dedc52ba0cfbbdfbdcda5a6c903ec2d
Sajad Torkamani <sajad@surelygroup.com>
2019-03-23 10:55:33 +0000

    Solve kata

---
+++ counter/solution.js
@@ -0,0 +1,13 @@
+function Counter() {
+  this.value = 0;
+}
+
+Counter.prototype.incr = function () {
+  this.value += 1;
+};
+
+Counter.prototype.valueOf = function () {
+  return this.value;
+};
+
+export default Counter;
