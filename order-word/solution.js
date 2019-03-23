503af05ec7048e06ee50a69aa3b29beb2c538dec
Sajad Torkamani <sajad@surelygroup.com>
2019-03-23 11:37:39 +0000

    Solve kata

---
+++ order-word/solution.js
@@ -0,0 +1,7 @@
+const orderWord = str => {
+  return typeof str === 'string' && str.length
+    ? str.split('').sort().join('')
+    : 'Invalid String!';
+};
+
+export default orderWord;
---
+++ order-word/test.js
@@ -0,0 +1,16 @@
+import orderWord from './solution';
+
+describe('orderWord', () => {
+  test('returns correct result', () => {
+    expect(orderWord("Hello, World!")).toEqual(" !,HWdellloor");
+    expect(orderWord("bobby")).toEqual("bbboy");
+    expect(orderWord("b")).toEqual("b");
+    expect(orderWord("")).toEqual("Invalid String!");
+    expect(orderWord("completesolution")).toEqual("ceeillmnooopsttu");
+    expect(orderWord("\"][@!$*(^&%")).toEqual("!\"$%&(*@[]^");
+    expect(orderWord("i\"d][@z!$r(^a&world%")).toEqual("!\"$%&(@[]^addilorrwz");
+    expect(orderWord(null)).toEqual("Invalid String!");
+    expect(orderWord("cba")).toEqual("abc");
+    expect(orderWord("abc")).toEqual("abc");
+  });
+});
