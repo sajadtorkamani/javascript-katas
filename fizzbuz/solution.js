990d06beb13418572fedd7f47858f3d83faba6aa
Sajad Torkamani <sajad@surelygroup.com>
2019-03-23 10:39:09 +0000

    Solve kata

---
+++ fizzbuz/solution.js
@@ -0,0 +1,17 @@
+const fizzbuzz = n => {
+  if (n % 5 === 0 && n % 3 === 0) {
+    return 'fizz buzz';
+  }
+
+  if (n % 3 === 0) {
+    return 'fizz';
+  }
+
+  if (n % 5 === 0) {
+    return 'buzz';
+  }
+
+  return n;
+};
+
+export default fizzbuzz;
