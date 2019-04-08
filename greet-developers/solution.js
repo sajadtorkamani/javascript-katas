3dab8b0ae19de54069fbc537314ae41e250c8346
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-04-08 12:44:47 +0100

    Solve kata

---
+++ greet-developers/solution.js
@@ -0,0 +1,8 @@
+const greetDevelopers = developers => {
+  return developers.map(developer => ({
+    ...developer,
+    greeting: `Hi ${developer.firstName}, what do you like the most about ${developer.language}?`
+  }));
+};
+
+export default greetDevelopers;
