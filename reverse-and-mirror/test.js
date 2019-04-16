2b277ffc2a042e10fbe86a00bc11aaa50e0ffd53
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-04-16 10:49:04 +0100

    Solve kata

---
+++ reverse-and-mirror/test.js
@@ -0,0 +1,20 @@
+import reverseAndMirror, { invertCasing, reverseString } from './solution';
+
+describe('reverseAndMirror', () => {
+  test('returns correct result', () => {
+    expect(reverseAndMirror('FizZ', 'buZZ')).toEqual('zzUB@@@zZIffIZz');
+    expect(reverseAndMirror('String Reversing', 'Changing Case')).toEqual('ESAc GNIGNAHc@@@GNISREVEr GNIRTssTRING rEVERSING');
+  });
+});
+
+describe('reverseString', () => {
+  test('reverses string correctly', () => {
+    expect(reverseString('foo')).toEqual('oof');
+  });
+});
+
+describe('invertCasing', () => {
+  test('returns correct result', () => {
+    expect(invertCasing('foo BAR BAz')).toEqual('FOO bar baZ');
+  });
+});
