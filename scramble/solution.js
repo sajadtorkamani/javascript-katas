a4e2e2d74f5aefbf169ef63bcab5d75ae2faabbb
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-04-28 20:20:11 +0700

    Solve kata

---
+++ scramble/solution.js
@@ -0,0 +1,15 @@
+const scramble = (str, positions) => {
+  let result = new Array(str.length).fill();
+  
+  str 
+    .split('')
+    .forEach((letter, index) => {
+      const position = positions[index];
+
+      result[position] = letter;
+    });
+
+  return result.join('');
+};
+
+export default scramble;
