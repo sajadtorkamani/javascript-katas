f0e98b666ad85f40f61985033da1c814f8bbb86f
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-04-28 21:58:28 +0700

    Solve kata

---
+++ sum-or-product/readme.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/larger-product-or-sum/train/javascript
---
+++ sum-or-product/solution.js
@@ -0,0 +1,19 @@
+const sumOrProduct = (nums, n) => {
+  const sortedNums = nums.sort((a, b) => a - b);
+
+  const sum = sortedNums
+    .slice(-(n))
+    .reduce((a, b) => a + b);
+
+  const product = sortedNums
+    .slice(0, n)
+    .reduce((a, b) => a * b);
+
+  switch (true) {
+    case sum > product: return 'sum';
+    case product > sum: return 'product';
+    default: return 'same';
+  }
+};
+
+export default sumOrProduct;
