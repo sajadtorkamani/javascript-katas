dbd7255feec683efe86f97a8ba7bc513ca5eb1b8
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-05-11 13:34:07 +0700

    Solve kata

---
+++ past/solution.js
@@ -0,0 +1,11 @@
+const past = (hours, mins, secs) => {
+  let milliseconds = 0;
+
+  milliseconds += secs * 1000;
+  milliseconds += mins * 60000;
+  milliseconds += hours * 3600000;
+
+  return milliseconds;
+};
+
+export default past;
