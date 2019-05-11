b474a3ef2dfce1310f216b7b2784d9a7bf50777c
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-05-11 17:43:01 +0700

    Solve kata

---
+++ encode/readme.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/digital-cypher/train/javascript
---
+++ encode/solution.js
@@ -0,0 +1,17 @@
+const encode = (str, keyNum) => {
+  const key = keyNum.toString().split('').map(Number);
+  let keyIndex = 0;
+
+  return str
+    .split('')
+    .map(letter => {
+      const num = letter.charCodeAt(0) - 96;
+      const encodedNum = num + key[keyIndex];
+
+      keyIndex = (keyIndex === key.length - 1) ? 0 : keyIndex + 1;
+
+      return encodedNum;
+    });
+};
+
+export default encode;
