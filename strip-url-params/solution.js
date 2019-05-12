bf88b9c466c2337f9a1cb7f658becaa7f6919d7b
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-05-12 13:08:06 +0700

    Solve kata

---
+++ strip-url-params/solution.js
@@ -0,0 +1,36 @@
+const qs = require('querystring');
+
+/**
+ * @param url {string}
+ * @param [paramsToStrip] {Array<string>}
+ */
+const stripUrlParams = (url, paramsToStrip = []) => {
+  const match = url.match(/(.+)\?(.+)/);
+
+  // Return unchanged url if no query params are present.
+  if (!match) {
+    return url;
+  }
+
+  const domain = match[1];
+  const paramsString = match[2];
+  let newParams = {};
+
+  // Remove duplicates
+  paramsString
+    .split('&')
+    .forEach(param => {
+      const [paramKey, paramValue] = param.split('=');
+
+      if (!newParams.hasOwnProperty(paramKey)) {
+        newParams[paramKey] = paramValue;
+      }
+    });
+
+  // Strip blacklisted params
+  paramsToStrip.forEach(param => delete newParams[param]);
+
+  return `${domain}?${qs.stringify(newParams)}`;
+};
+
+export default stripUrlParams;
---
+++ strip-url-params/test.js
@@ -0,0 +1,9 @@
+import stripUrlParams from './solution';
+
+describe('stripUrlParams', () => {
+  test('strips duplicate url params', () => {
+    expect(stripUrlParams('www.codewars.com?a=1&b=2&a=2')).toEqual('www.codewars.com?a=1&b=2');
+    expect(stripUrlParams('www.codewars.com?a=1&b=2&a=2', ['b'])).toEqual('www.codewars.com?a=1');
+    expect(stripUrlParams('www.codewars.com', ['b'])).toEqual('www.codewars.com');
+  });
+});
