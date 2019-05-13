89a71bbdf20a598e4d8d8893b91ea1a56ff88019
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-05-13 16:46:25 +0700

    Solve kata

---
+++ array-dedup/readme.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/no-duplicates-here/train/javascript
---
+++ array-dedup/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param arr {Array}
+ */
+const arrayDeDup = arr => {
+  if (!Array.isArray(arr)) {
+    return 'Not an array';
+  }
+
+  let list = [];
+
+  // Filter unique & truthy items
+  arr.forEach(item => {
+    if (item && !list.includes(item)) {
+      list.push(item);
+    }
+  });
+
+  return list.sort();
+};
+
+export default arrayDeDup;
