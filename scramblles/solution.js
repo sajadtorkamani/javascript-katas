8276c0bb3f0f0f603f9370d144bcefa0c51940c4
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-05-13 13:12:19 +0700

    Solve kata

---
+++ scramblles/solution.js
@@ -0,0 +1,26 @@
+/**
+ * @param s1 {string}
+ * @param s2 {String}
+ */
+const scramble = (s1, s2) => {
+  let letterCounts = {};
+
+  s2
+    .split('')
+    .forEach(letter => {
+      letterCounts[letter] = letterCounts.hasOwnProperty(letter) ? letterCounts[letter] + 1 : 1;
+    });
+
+  for (let letter in letterCounts) {
+    const s2LetterCount = letterCounts[letter];
+    const s1LetterCount = (s1.match(new RegExp(letter, 'g')) || []).length;
+
+    if (s1LetterCount < s2LetterCount) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+export default scramble;
---
+++ scramblles/test.js
@@ -0,0 +1,16 @@
+import scramble from './solution';
+
+describe('scramble', () => {
+  test('returns correct result', () => {
+    expect(scramble('scriptjava', 'javascript')).toEqual(true);
+    expect(scramble('rkqodlw', 'world')).toEqual(true);
+    expect(scramble('cedewaraaossoqqyt', 'codewars')).toEqual(true);
+    expect(scramble('scriptjava', 'javascript')).toEqual(true);
+    expect(scramble('scriptingjava', 'javascript')).toEqual(true);
+    expect(scramble('scriptsjava', 'javascripts')).toEqual(true);
+    expect(scramble('aabbcamaomsccdd', 'commas')).toEqual(true);
+
+    expect(scramble('jscripts', 'javascript')).toEqual(false);
+    expect(scramble('katas', 'steak')).toEqual(false);
+  });
+});
