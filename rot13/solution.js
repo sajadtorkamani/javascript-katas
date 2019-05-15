e4a6f1dc11269a246861fa4556e580234d9a9002
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-05-15 14:20:17 +0700

    Solve kata

---
+++ rot13/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param str {string}
+ */
+const rot13 = str => str.replace(/[A-Za-z]/g, encryptLetter);
+
+/**
+ * @param letter {string}
+ * @return {string}
+ */
+export const encryptLetter = letter => {
+  const alphabet = /[A-Z]/.test(letter) ? 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' : 'abcdefghijklmnopqrstuvwxyz';
+  const nextLetterPos = alphabet.indexOf(letter) + 13;
+
+  return nextLetterPos >= alphabet.length
+    ? alphabet.charAt(nextLetterPos % alphabet.length)
+    : alphabet.charAt(nextLetterPos);
+};
+
+export default rot13;
