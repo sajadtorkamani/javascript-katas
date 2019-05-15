451d870e203a8902a88dc1f3a12a97d9dfc7f5de
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-05-15 18:21:21 +0700

    Solve kata

---
+++ search-substr/readme.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/return-substring-instance-count-2/train/javascript
---
+++ search-substr/solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param haystack {string}
+ * @param needle {string}
+ * @param allowOverlap {boolean}
+ * @return {number}
+ */
+function searchSubstr(haystack, needle, allowOverlap = true) {
+  if (!needle) {
+    return 0;
+  }
+
+  const regex = allowOverlap && isAllSameChars(needle)
+    ? new RegExp(`${needle[0]}(?=${needle[0].repeat(needle.length - 1)})`, 'g')
+    : new RegExp(needle, 'g');
+
+  return (haystack.match(regex) || []).length;
+};
+
+/**
+ * @param str {string}
+ * @return {boolean}
+ */
+const isAllSameChars = str => {
+  return new RegExp('^' + str[0] + '+$').test(str);
+};
+
+export default searchSubstr;
