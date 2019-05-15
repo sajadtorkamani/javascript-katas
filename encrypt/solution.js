d57d3017e0f262fdd4f54e09d3253207083bfb38
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-05-15 17:41:03 +0700

    Solve kata

---
+++ encrypt/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param text {string}
+ * @param rule {number}
+ */
+const encrypt = (text, rule) => {
+  return text.replace(/./g, char => {
+    const charCode = char.charCodeAt(0) + rule;
+
+    return charCode > 255
+      ? String.fromCharCode(charCode % 256)
+      : String.fromCharCode(charCode);
+  });
+};
+
+export default encrypt;
