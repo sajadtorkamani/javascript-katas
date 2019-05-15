5ff2cc9ee7e3c293c90780882f21d54e3af1bfda
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-05-15 15:10:55 +0700

    Solve kata

---
+++ make-sentence/solution.js
@@ -0,0 +1,11 @@
+/**
+ * @param parts {Array<string>}
+ */
+const makeSentence = parts => {
+  return parts
+    .filter(p => p !== '.')
+    .join(' ')
+    .replace(/ ,/g, ',') + '.';
+};
+
+export default makeSentence;
---
+++ make-sentence/test.js
@@ -0,0 +1,9 @@
+import makeSentence from './solution';
+
+describe('makeSentence', () => {
+  test('returns correctly formed sentence from given parts', () => {
+    expect(makeSentence(['hello', 'world'])).toEqual('hello world.');
+    expect(makeSentence([ 'hello', ',', 'my', 'dear' ])).toEqual('hello, my dear.');
+    expect(makeSentence(['hello', 'world', '.', '.', '.'])).toEqual('hello world.');
+  });
+});
