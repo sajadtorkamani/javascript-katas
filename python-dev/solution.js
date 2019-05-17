2432be1c4409c277f2042f24575ebc96818bd347
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-05-17 13:29:26 +0700

    Solve kata

---
+++ python-dev/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param list {Array<{}>}
+ * @return {string}
+ */
+const getFirstPython = list => {
+  const pythonDev = list.find(dev => dev.language === 'Python');
+
+  return pythonDev
+    ? `${pythonDev.firstName}, ${pythonDev.country}`
+    : 'There will be no Python developers';
+};
+
+export default getFirstPython;
---
+++ python-dev/test.js
@@ -0,0 +1,18 @@
+import getFirstPython from './solution';
+
+describe('getFirstPython', () => {
+  test('returns correct result', () => {
+    const list1 = [
+      { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+      { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+      { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+    ];
+    const list2 = [
+      { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
+      { firstName: 'Amar', lastName: 'V.', country: 'Bosnia and Herzegovina', continent: 'Europe', age: 32, language: 'Ruby' }
+    ];
+
+    expect(getFirstPython(list1)).toEqual('Victoria, Puerto Rico');
+    expect(getFirstPython(list2)).toEqual('There will be no Python developers');
+  });
+});
