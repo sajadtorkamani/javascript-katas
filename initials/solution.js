a4aebb2693e692e10053fd0fa3e2150a26e4fd67
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-05-17 19:40:23 +0700

    Solve kata

---
+++ initials/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} name
+ * @return {string}
+ */
+const initials = name => {
+  return name
+    .split(' ')
+    .map((part, index, parts) => {
+      return index === parts.length - 1
+        ? part[0].toUpperCase() + part.substr(1).toLowerCase()
+        : part[0].toUpperCase();
+    })
+    .join('.')
+};
+
+export default initials;
---
+++ initials/test.js
@@ -0,0 +1,9 @@
+import initials from './solution';
+
+describe('initials', () => {
+  test('returns correct result', () => {
+    expect(initials('code wars')).toEqual('C.Wars');
+    expect(initials('Barack hussain obama')).toEqual('B.H.Obama');
+    expect(initials('barack hussain Obama')).toEqual('B.H.Obama');
+  });
+});
