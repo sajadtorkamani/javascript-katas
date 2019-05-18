59f1bfa0b0bbdcd49c0690d42991aa6b2d323d84
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-05-18 18:54:58 +0700

    Solve kata

---
+++ validate-credit-card-num/readme.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/validate-credit-card-number/train/javascript
---
+++ validate-credit-card-num/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+const validate = num => {
+  return num
+    .toString()
+    .split('')
+    .reverse()
+    .map((digit, index) => index % 2 === 1 ? +digit * 2 : +digit)
+    .map(digit => digit > 9 ? digit - 9 : digit)
+    .reduce((sum, digit) => sum + digit) % 10 === 0;
+};
+
+export default validate;
