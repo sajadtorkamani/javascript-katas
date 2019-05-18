20d90d0ceb6a56f8b8a86d04cdba154d31994206
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-05-18 21:01:50 +0700

    Solve kata

---
+++ stock-list/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Array<string>} stock
+ * @param {Array<string>} categories
+ * @return {string}
+ */
+const stockList = (stock, categories) => {
+  if (!stock.length || !categories.length) {
+    return '';
+  }
+
+  return categories
+    .map(categoryCode => {
+      const quantity = stock
+        .filter(itemCode => itemCode.startsWith(categoryCode))
+        .reduce((total, item) => total + +item.match(/\d+/)[0], 0);
+
+      return `(${categoryCode} : ${quantity})`;
+    })
+    .join(' - ');
+};
+
+export default stockList;
