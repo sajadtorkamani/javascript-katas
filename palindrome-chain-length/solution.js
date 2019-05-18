43daee891b3c413208ada8000abdcf71f4cecb0c
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-05-18 17:12:18 +0700

    Solve kata

---
+++ palindrome-chain-length/solution.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+const palindromeChainLength = num => {
+  if (isPalindrome(num)) {
+    return 0;
+  }
+
+  let numSteps = 0;
+  let sum = num;
+
+  while (true) {
+    sum = sum + getReversedNum(sum);
+    numSteps += 1;
+
+    if (isPalindrome(sum)) {
+      return numSteps;
+    }
+  }
+};
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+export const isPalindrome = num => {
+  return getReversedNum(num) === num;
+};
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+export const getReversedNum = num => {
+  return +num
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+};
+
+export default palindromeChainLength;
---
+++ palindrome-chain-length/test.js
@@ -0,0 +1,22 @@
+import palindromeChainLength, { getReversed, getReversedNum, isPalindrome } from './solution';
+
+describe('palindromeChainLength', () => {
+  test('returns correct result', () => {
+    expect(palindromeChainLength(87)).toEqual(4);
+    expect(palindromeChainLength(101)).toEqual(0);
+  });
+});
+
+describe('isPalindrome', () => {
+  test('returns true if input is a palindrome', () => {
+    expect(isPalindrome(1221)).toEqual(true);
+
+    expect(isPalindrome(1223)).toEqual(false);
+  });
+});
+
+describe('getReversedNum', () => {
+  test('returns reversed equivalent of num', () => {
+    expect(getReversedNum(1234)).toEqual(4321);
+  });
+});
