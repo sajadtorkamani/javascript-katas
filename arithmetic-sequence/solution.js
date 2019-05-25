26fa9c6d137d896a5d2621afe0061409a2b1c7c8
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-05-25 18:01:09 +0700

    Solve kata

---
+++ arithmetic-sequence/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} first
+ * @param {number} diff
+ * @param {number} count
+ * @return {string}
+ */
+const arithmeticSequenceElements = (first, diff, count) => {
+  let sequence = [first];
+
+  for (let position = 1; position <= (count - 1); position++) {
+    const num = first + (position * diff);
+    sequence.push(num);
+  }
+
+  return sequence.join(', ');
+};
+
+export default arithmeticSequenceElements;
---
+++ arithmetic-sequence/test.js
@@ -0,0 +1,10 @@
+import arithmeticSequenceElements from './solution';
+
+describe('arithmeticSequenceElements', () => {
+  test('returns correct result', () => {
+    expect(arithmeticSequenceElements(1, 2, 5)).toEqual('1, 3, 5, 7, 9');
+    expect(arithmeticSequenceElements(1, 0, 5)).toEqual('1, 1, 1, 1, 1');
+    expect(arithmeticSequenceElements(1, -3, 10)).toEqual('1, -2, -5, -8, -11, -14, -17, -20, -23, -26');
+    expect(arithmeticSequenceElements(100, -10, 10)).toEqual('100, 90, 80, 70, 60, 50, 40, 30, 20, 10');
+  });
+});
