9ee9e2cf9807588cfa8e21db39578f495abe7dbb
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-05-26 09:09:18 +0800

    Solve kata

---
+++ two-sum/test.js
@@ -0,0 +1,9 @@
+import twoSum from './solution';
+
+describe('twoSum', () => {
+  test('returns the indices of the two elements that when added together give `target`', () => {
+    expect(twoSum([1, 2, 3], 4)).toEqual([0, 2]);
+    expect(twoSum([1234, 5678, 9012], 14690)).toEqual([1, 2]);
+    expect(twoSum([2, 2, 3], 4)).toEqual([0, 1]);
+  });
+});
