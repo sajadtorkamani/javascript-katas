7ae70cb0e23fb9c96cec5198a28adc64c087ad15
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-06-13 10:54:37 +0100

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,12 @@
 module.exports = {
-  "env": {
-    "node": true,
-    "es6": true,
-    "jest": true
+  env: {
+    node: true,
+    es6: true,
+    jest: true
   },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module"
-  },
-  "rules": {
-    "indent": [
-      "error",
-      2,
-      {
-        "SwitchCase": 1
-      }
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ]
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
   }
 };
