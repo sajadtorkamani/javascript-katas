173d0050dc32dbf186c8beb65cbea5f233f10483
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-06-13 12:21:47 +0100

    Solve kata

---
+++ small-words-helper/readme.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/make-the-small-words-big/train/javascript
---
+++ small-words-helper/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} str
+ * @returns {string}
+ */
+const smallWordHelper = str => {
+  return str
+    .split(' ')
+    .map(word => {
+      return word.length <= 3
+        ? word.toUpperCase()
+        : word.replace(/[aeiou]/gi, '');
+    })
+    .join(' ');
+};
+
+export default smallWordHelper;
