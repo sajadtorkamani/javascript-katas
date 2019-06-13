2f5d8a1854390045e949b01404cc1019c11de7c4
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-06-13 11:52:48 +0100

    Solve kata

---
+++ tic-tac-toe/readme.md
@@ -0,0 +1,3 @@
+# This solution needs major refactoring :)
+
+https://www.codewars.com/kata/525caa5c1bf619d28c000335
---
+++ tic-tac-toe/test.js
@@ -0,0 +1,68 @@
+import isSolved from './solution';
+
+describe.only('isSolved', () => {
+  test('detects horizontal win', () => {
+    // prettier-ignore
+    const board = [
+      ['2', '2', '1'],
+      ['1', '1', '1'],
+      ['1', '2', '2']
+    ];
+
+    expect(isSolved(board)).toEqual(1);
+  });
+
+  test('detects vertical win', () => {
+    // prettier-ignore
+    const board = [
+      ['2', '2', '1'],
+      ['1', '2', '2'],
+      ['1', '2', '1']
+    ];
+
+    expect(isSolved(board)).toEqual(2);
+  });
+
+  test('detects forward diagonal win', () => {
+    // prettier-ignore
+    const board = [
+      ['2', '2', '1'],
+      ['2', '1', '2'],
+      ['1', '1', '2']
+    ];
+
+    expect(isSolved(board)).toEqual(1);
+  });
+
+  test('detects reverse diagonal win', () => {
+    // prettier-ignore
+    const board = [
+      ['2', '2', '1'],
+      ['2', '2', '1'],
+      ['1', '1', '2']
+    ];
+
+    expect(isSolved(board)).toEqual(2);
+  });
+
+  test("returns -1 if it's not finished", () => {
+    // prettier-ignore
+    const board = [
+    ['2', '1', '1'],
+    ['1', '2', '0'],
+    ['0', '1', '0']
+  ];
+
+    expect(isSolved(board)).toEqual(-1);
+  });
+  test("returns 0 if it's a draw", () => {
+    // prettier-ignore
+    const board = [
+    ['2', '1', '1'],
+    ['1', '2', '2'],
+    ['2', '1', '1']
+  ];
+
+    expect(isSolved(board)).toEqual(0);
+  });
+});
