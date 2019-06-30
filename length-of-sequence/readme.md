8dcbb4afedefa5155188d2cd680926b8e38f5100
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-06-30 07:40:23 +0100

    Solve kata

---
+++ length-of-sequence/readme.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/finding-length-of-the-sequence/train/javascript
---
+++ length-of-sequence/solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @returns {mumber}
+ */
+export const lengthOfSequence = (nums, n) => {
+  const count = nums.filter(v => v === n).length;
+  if (count !== 2) return 0;
+
+  const firstIndex = nums.indexOf(n);
+  const lastIndex = nums.lastIndexOf(n);
+
+  return lastIndex - firstIndex + 1;
+};
