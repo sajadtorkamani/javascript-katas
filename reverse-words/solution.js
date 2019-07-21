059b781875a8671356890e5c9d6d2dae0e67a904
Sajad Torkamani <sajadtorkamani1@gmail.co>
2019-07-21 10:48:11 +0100

    Solve kata

---
+++ reverse-words/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} str
+ * @returns {string}
+ */
+const reverse = str => {
+  return str
+    .trim()
+    .split(' ')
+    .map((word, index) => {
+      return index % 2 === 1
+        ? word.split('').reverse().join('') // prettier-ignore
+        : word;
+    })
+    .join(' ');
+};
+
+export default reverse;
