4682f2f26164944b517b757317c82f8e7d932713
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-07-21 13:02:06 +0100

    Solve kata

---
+++ is-circle-sorted/readme.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/circularly-sorted-array/train/javascript
---
+++ is-circle-sorted/test.js
@@ -0,0 +1,10 @@
+import isCircleSorted from './solution';
+
+describe('isCircleSorted', () => {
+  test('returns correct result', () => {
+    // expect(isCircleSorted([2, 3, 4, 5, 6])).toEqual(true);
+    expect(isCircleSorted([10, 11, 6, 7, 9])).toEqual(true);
+    expect(isCircleSorted([6, 2, 3, 4, 5])).toEqual(true);
+    expect(isCircleSorted([3, 2, 4, 5, 6])).toEqual(false);
+  });
+});
