ed9f79f6f7c8135d19ecb9fe6b1691394dc9d00e
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-07-29 00:17:10 +0100

    Solve kata

---
+++ group/test.js
@@ -0,0 +1,8 @@
+import group from './solution';
+
+describe('group', () => {
+  test('returns correct result', () => {
+    expect(group([3, 2, 6, 2, 1, 3])).toEqual([[3, 3], [2, 2], [6], [1]]);
+    // expect(group([3, 2, 6, 2])).toEqual([[3], [2, 2], [6]]);
+  });
+});
