0d3b23be5a2640846cb87de6956faeafd62012b1
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-08-09 09:27:31 +0100

    Solve kata

---
+++ args/solution.js
@@ -0,0 +1,15 @@
+const args = cmd => {
+  return cmd
+    .trim()
+    .replace(/\s*[|>;&].*/, '')
+    .split(' ');
+};
+
+export default args;const args = cmd => {
+  return cmd
+    .trim()
+    .replace(/\s*[|>;&].*/, '')
+    .split(' ');
+};
+
+export default args;
