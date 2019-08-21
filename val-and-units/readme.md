78eb7e0d73b639ec8c0796c17b077c7c4995a038
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-08-21 14:18:12 +0100

    Solve kata

---
+++ val-and-units/readme.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/extract-values-and-units/train/javascript
---
+++ val-and-units/solution.js
@@ -0,0 +1,9 @@
+// TODO: could be simpler
+const valAndUnits = str => {
+  const units = (str.match(/[^0-9]+$/i) || [])[0] || '';
+  const val = +str.replace(units, '');
+
+  return { val, units: units.trim() };
+};
+
+export default valAndUnits;
