a482950ee63d60c79cc6a4cc3eea75d812de1e3c
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-09-01 15:54:18 +0100

    Refactor solution

--- insert-dash-2/solution.js
+++ insert-dash-2/solution.js
@@ -1,32 +1,8 @@
 const insertDashII = num => {
-  let numStr = num.toString();
-  const oddNumsRegex = /[13579](?=([13579]))/g;
-  const evenNumsRegex = /[2468](?=([2468]))/g;
-  let oddPairs = [];
-  let evenPairs = [];
-  let match;
-
-  // Find odd pairs
-  while ((match = oddNumsRegex.exec(numStr))) {
-    oddPairs.push(match[0] + match[1]);
-  }
-
-  // Find even pairs
-  while ((match = evenNumsRegex.exec(numStr))) {
-    evenPairs.push(match[0] + match[1]);
-  }
-
-  // Replace odd pairs
-  oddPairs.forEach(oddPair => {
-    numStr = numStr.replace(oddPair, m => m[0] + '-' + m[1]);
-  });
-
-  // Replace even pairs
-  evenPairs.forEach(evenPair => {
-    numStr = numStr.replace(evenPair, m => m[0] + '*' + m[1]);
-  });
-
-  return numStr;
+  return num
+    .toString()
+    .replace(/([13579])(?=([13579]))/g, '$1-')
+    .replace(/([2468])(?=([2468]))/g, '$1*');
 };
 
 export default insertDashII;
