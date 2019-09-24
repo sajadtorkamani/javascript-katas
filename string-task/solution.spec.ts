b23b60a11ac7f823dfd2dcc4f8adbe9858d58c56
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-09-24 13:33:19 +0100

    Add Typescript to project

---
+++ string-task/solution.spec.ts
@@ -0,0 +1,9 @@
+import stringTask from "./solution";
+
+describe('stringTask', () => {
+  test('returns correct result', () => {
+    expect(stringTask("tour")).toEqual(".t.r");
+    expect(stringTask('Codewars')).toEqual('.c.d.w.r.s');
+    expect(stringTask('aBAcAba')).toEqual('.b.c.b');
+  });
+});
---
+++ string-task/solution.ts
@@ -0,0 +1,8 @@
+const stringTask = (str: string): string => {
+  return str
+    .toLowerCase()
+    .replace(/[aeiouy]/g, '')
+    .replace(/\w/g, '.' + '$&');
+};
+
+export default stringTask;
