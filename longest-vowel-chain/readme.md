6aea93177efa7a6a1608e7e88ab637a0b61528af
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-09-25 10:21:31 +0100

    Solve kata

---
+++ longest-vowel-chain/readme.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/longest-vowel-chain/train/javascript
---
+++ longest-vowel-chain/solution.ts
@@ -0,0 +1,13 @@
+const longestVowelChain = (str: string): number => {
+  const vowelChains = str.match(/[aeiou]+/g) || [];
+
+  if (vowelChains.length === 0) {
+    return 0;
+  }
+
+  const lengths = vowelChains.map(v => v.length);
+
+  return Math.max(...lengths);
+};
+
+export default longestVowelChain;
