e3d5132cf686a5b35c9ef8c1a45a16d581a6f72a
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-10-23 18:13:33 +0100

    Solve kata

---
+++ sort-by-example/solution.ts
@@ -0,0 +1,5 @@
+const exampleSort = <T>(arr: T[], exampleArr: T[]): T[] => {
+  return arr.sort((a, b) => exampleArr.indexOf(a) - exampleArr.indexOf(b));
+};
+
+export default exampleSort;
---
+++ sort-by-example/test.ts
@@ -0,0 +1,10 @@
+import exampleSort from './solution';
+
+describe('sortByExample', () => {
+  test('returns correct result', () => {
+    expect(exampleSort<number>([1,2,3,4,5],[2,3,4,1,5])).toEqual([2,3,4,1,5]);
+    expect(exampleSort<number>([1,2,3,3,3,4,5],[2,3,4,1,5])).toEqual([2,3,3,3,4,1,5]);
+    expect(exampleSort<number>([1,2,3,3,3,5],[2,3,4,1,5])).toEqual([2,3,3,3,1,5]);
+    expect(exampleSort<number>([1,2,3,3,3,5],[3,4,5,6,9,11,12,13,1,7,8,2,10])).toEqual([3,3,3,5,1,2]);
+  });
+});
