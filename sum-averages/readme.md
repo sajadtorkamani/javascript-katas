e8fb6201d2affaa3f07a0955d1d9fac7f7bb9bcf
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-10-23 14:31:30 +0100

    Solve kata

---
+++ sum-averages/readme.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/sum-of-array-averages/train/javascript
---
+++ sum-averages/solution.ts
@@ -0,0 +1,9 @@
+const sumAverage = (arrs: number[][]): number => {
+  const sum = arrs.map(arr => arr.reduce(sumNums) / arr.length).reduce(sumNums);
+
+  return Math.floor(sum);
+};
+
+const sumNums = (a: number, b: number) => a + b;
+
+export default sumAverage;
