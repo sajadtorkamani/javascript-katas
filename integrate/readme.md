a803704575b9f10f979bde44cf98a21a1a7711d0
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-11-02 12:01:49 +0000

    Solve kata

---
+++ integrate/readme.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/find-the-integral/train/javascript
---
+++ integrate/solution.ts
@@ -0,0 +1,8 @@
+const integrate = (coefficient: number, exponent: number): string => {
+  const a = coefficient / (exponent + 1);
+  const b = exponent + 1;
+
+  return a + 'x^' + b;
+};
+
+export default integrate;
