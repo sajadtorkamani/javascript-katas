48d2d6e356db6eaf4f50d64d2ac90bc4cfa435b5
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-11-02 12:32:33 +0000

    Solve kata

---
+++ flip-number/readme.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/reversing-fun/train/javascript
---
+++ flip-number/solution.ts
@@ -0,0 +1,13 @@
+const flipNumber = (num: string): string => {
+  let digits = num.split('');
+  let flippedDigits = [];
+
+  while (digits.length) {
+    flippedDigits.push(digits.pop());
+    flippedDigits.push(digits.shift());
+  }
+
+  return flippedDigits.filter(Boolean).join('');
+};
+
+export default flipNumber;
