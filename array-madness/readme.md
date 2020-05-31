5008a12892533a0f66d51e30fd254176d53eef33
Sajad Torkamani <sajadtorkamani1@gmail.com>
2020-05-31 06:27:30 +0100

    Solve kata

---
+++ array-madness/readme.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/56ff6a70e1a63ccdfa0001b1/train/javascript
---
+++ array-madness/test.js
@@ -0,0 +1,7 @@
+import { arrayMadness } from './solution';
+
+describe('arrayMadness', () => {
+  test('returns correct result', () => {
+    expect(arrayMadness([4, 5, 6], [1, 2, 3])).toBe(true);
+  });
+});
