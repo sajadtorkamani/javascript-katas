54b01b190fc3fcc3a7aacfe064d2ec6b02b9624e
Sajad Torkamani <sajadtorkamani1@gmail.com>
2020-06-07 10:33:03 +0100

    Solve kata

---
+++ number-joy/readme.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/570523c146edc287a50014b1/train/javascript
---
+++ number-joy/solution.js
@@ -0,0 +1,26 @@
+export const isHarshadNum = num => {
+  return num % getDigitsSum(num) === 0;
+};
+
+export const getDigitsSum = num => {
+  return num
+    .toString()
+    .split('')
+    .reduce((a, b) => a + Number(b), 0);
+};
+
+export const reverseNum = num => {
+  const reversed = num
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+
+  return Number(reversed);
+};
+
+export const numberJoy = num => {
+  const digitsSum = getDigitsSum(num);
+
+  return isHarshadNum(num) && digitsSum * reverseNum(digitsSum) === num;
+};
