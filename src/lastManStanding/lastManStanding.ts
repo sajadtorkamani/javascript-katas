84a50144f146c03f5260d20983882a20d2ef0ab7
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-04-29 15:26:47 +0100

    Solve kata

--- src/lastManStanding/lastManStanding.ts
+++ src/lastManStanding/lastManStanding.ts
@@ -1,33 +1,29 @@
-export function lastManStanding(n: number): number {
-  let numbers = new Array(n).fill(undefined).map((_value, index) => index + 1)
-  let iterationCount = 0
+export function lastManStanding(numbersLength: number): number {
+  let numbers = createNumbers(numbersLength)
   let direction = 'left-to-right'
 
-  // Starting from the right, remove every other number)
   while (numbers.length > 1) {
     if (direction === 'left-to-right') {
-      numbers = numbers
-        .map((num, index) => {
-          return index % 2 === 0 ? null : num
-        })
-        .filter(Boolean)
+      numbers = numbers.map(removeEveryOtherNum)
       direction = 'right-to-left'
     } else {
       numbers = numbers
         .reverse()
-        .map((num, index) => {
-          return index % 2 === 0 ? null : num
-        })
-        .filter(Boolean)
+        .map(removeEveryOtherNum)
         .reverse()
       direction = 'left-to-right'
     }
 
-    iterationCount += 1
-    console.log(`After iteration ${iterationCount}: ${numbers}`)
+    numbers = numbers.filter(Boolean)
   }
 
-  console.log({ numbers })
-
   return numbers[0]
+}
+
+function createNumbers(length: number) {
+  return new Array(length).fill().map((_, index) => index + 1)
+}
+
+function removeEveryOtherNum(num: number, index: number) {
+  return index % 2 === 0 ? null : num
 }
